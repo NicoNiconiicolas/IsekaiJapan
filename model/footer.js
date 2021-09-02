e0ba7def4ad92f99b3b75afbafd1945fd6ce9ac3
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-03 01:26:18 +0200

    Add files via upload

--- model/footer.js
+++ model/footer.js
@@ -2,7 +2,9 @@ document.getElementsByTagName('body')[0].innerHTML += "<div id='footer'>"+
 "<table>"+
 "<tr><th>Jlpt</th><th>Pages</th><th>Contacts</th></tr>"+
 "<tr><td onclick='goto(\"n5\")'>N5</td><td onclick='goto(\"home\")'>Accueil</td><td>Mail</td></tr>"+
-"<tr><td></td><td onclick='goto(\"initiation\")'>Initiation</td></tr>"+
-"<tr><td></td><td onclick='goto(\"outils\")'>Outils</td></tr>"+
+"<tr><td onclick='goto(\"404\")'>N4</td><td onclick='goto(\"initiation\")'>Initiation</td></tr>"+
+"<tr><td onclick='goto(\"404\")'>N3</td><td onclick='goto(\"outils\")'>Outils</td></tr>"+
+"<tr><td onclick='goto(\"404\")'>N2</td></tr>"+
+"<tr><td onclick='goto(\"404\")'>N1</td></tr>"+
 "</table>"+
 "</div>";
--- model/hooter.js
+++ model/hooter.js
@@ -2,6 +2,10 @@ document.getElementsByTagName('body')[0].innerHTML += "<img id='logo' src='img/b
 "<div id='menu'>"+
 "<div id='home' class='menu-el' onclick='goto(\"home\")'>Accueil</div>"+
 "<div id='initiation' class='menu-el' onclick='goto(\"initiation\")'>Initiation</div>"+
-"<div id='n5' class='menu-el' onclick='goto(\"n5\")'>JLPT N5</div>"+
-"<div id='outils' class='menu-el' onclick='goto(\"outils\")'>Outils</div>"+
+"<div id='outils' class='menu-el' onclick='goto(\"outils\")'>Outils</div></div>"+
+"<div id='menu2'><div id='n5' class='menu-el' onclick='goto(\"n5\")'>JLPT N5</div>"+
+"<div id='n4' class='menu-el' onclick='goto(\"404\")'>JLPT N4</div>"+
+"<div id='n3' class='menu-el' onclick='goto(\"404\")'>JLPT N3</div>"+
+"<div id='n2' class='menu-el' onclick='goto(\"404\")'>JLPT N2</div>"+
+"<div id='n1' class='menu-el' onclick='goto(\"404\")'>JLPT N1</div>"+
 "</div>";

7b7b9a26f5fb86a8be4f50e75fa30727d52b740c
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-09 16:51:04 +0200

    Add files via upload

---
+++ pages/n5/26.js
@@ -0,0 +1,44 @@
+document.getElementById('content').innerHTML = ""+
+"<div id='main'><div id='num'></div>"+
+"<h1>いつも<br>itsumo</h1>"+
+"<br><p><span class='gbox'>Sens</span></p><br>"+
+"<p>toujours, souvent, habituellement</p>"+
+"<br><p><span class='gbox'>Utilisation</span></p><br>"+
+"<p>En milieu de phrase ou pour commencer une nouvelle phrase</p>"+
+"<br><p>いつも peut exprimer :</p>"+
+"<br><p>1) Une chose qui arrive tout le temps</p>"+
+"<br><p>2) Une chose qui arrive souvent</p>"+
+"<br><p>3) une habitude</p>"+
+"<br><h3>exemple #1</h3>"+
+"<div class='ex'>彼は<span class='b'>いつも</span>元気ですね。</div>"+
+"<div class='hira'>かれはいつもげんきですね。</div>"+
+"<div class='trad'>Il est toujours énergique.</div>"+
+"<br><h3>exemple #2</h3>"+
+"<div class='ex'>妹は<span class='b'>いつも</span>寝ている。</div>"+
+"<div class='hira'>いもうとはいつもねている。</div>"+
+"<div class='trad'>Ma petite soeur est tout le temps en train de dormir.</div>"+
+"<br><h3>exemple #3</h3>"+
+"<div class='ex'><span class='b'>いつも</span>夜10時に寝ます。</div>"+
+"<div class='hira'>いつもよる１０じにねます。</div>"+
+"<div class='trad'>Je vais toujours me coucher à 10h le soir.</div>"+
+"<br><h3>exemple #4</h3>"+
+"<div class='ex'>昼ご飯は<span class='b'>いつも</span>納豆を食べます。</div>"+
+"<div class='hira'>ひるごはんはいつもなっとうをたべます。</div>"+
+"<div class='trad'>Je mange toujours du natto pour le déjeuner.</div>"+
+"<br><h3>exemple #5</h3>"+
+"<div class='ex'>仕事に行く前に<span class='b'>いつも</span>ジムに行きます。</div>"+
+"<div class='hira'>しごとにいくまえにいつもジムにいきます。</div>"+
+"<div class='trad'>Je vais toujours à la gym avant d'aller au travail.</div>"+
+"<br><h3>exemple #6</h3>"+
+"<div class='ex'><span class='b'>いつも</span>自転車で学校に来ますが今日は歩きました。</div>"+
+"<div class='hira'>いつもじてんしゃでがっこうにきますがきょうはあるきました。</div>"+
+"<div class='trad'>D'habitude je vais à l'école en vélo, mais aujourd'hui j'ai marché.</div>"+
+"<br><h3>exemple #7</h3>"+
+"<div class='ex'>今日は<span class='b'>いつも</span>と同じ一日だった。</div>"+
+"<div class='hira'>きょうはいつもとおなじいちにちだった。</div>"+
+"<div class='trad'>Aujourd'hui était un jour comme les autres.</div>"+
+"<br><h3>exemple #8</h3>"+
+"<div class='ex'>君と<span class='b'>いつも</span>一緒に居たい。</div>"+
+"<div class='hira'>きみといつもいっしょにいたい。</div>"+
+"<div class='trad'>Je veux toujours être avec toi.</div>"+
+"<br></div>";

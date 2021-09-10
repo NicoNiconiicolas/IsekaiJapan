5bedc2a7ece5455a2072cfe82d6f5a4efd3a2b57
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-10 15:14:58 +0200

    Add files via upload

--- pages/n5/2.js
+++ pages/n5/2.js
@@ -8,19 +8,21 @@ document.getElementById('content').innerHTML = ""+
 "<div class='conj'>"+
 	"<p><span class='b'>Présent: </span>だ</p>"+
 	"<p><span class='b'>Passé: </span>だった</p>"+
-	"<p><span class='b'>Présent négatif: </span>ではない</p>"+
-	"<p><span class='b'>Passé négatif: </span>ではなかった</p>"+
+	"<p><span class='b'>Présent négatif: <br></span>じゃない ｜ ではない</p>"+
+	"<p><span class='b'>Passé négatif: <br></span>じゃなかった　｜ ではなかった</p>"+
 "</div>"+
 "<br><p>Conjugaison de です (forme polie de だ)</p>"+
 "<div class='conj'>"+
 	"<p><span class='b'>Présent: </span>です</p>"+
 	"<p><span class='b'>Passé: </span>でした</p>"+
-	"<p><span class='b'>Présent négatif: </span>ではありません</p>"+
-	"<p><span class='b'>Passé négatif: </span>ではありませんでした</p>"+
+	"<p><span class='b'>Présent négatif: <br></span>じゃないです<br>じゃありません<br>ではありません<br></p>"+
+	"<p><span class='b'>Passé négatif: <br></span>じゃなかったです<br>ではありませんでした<br>じゃありませんでした</p>"+
 "</div>"+
 "<br><p class='pi'>・Il n'est pas rare que le だ sois omis en fin de phrase.</p>"+
 "<p class='pi'>・です est souvent ajouté en fin de phrase pour la rendre polie.</p>"+
 "<p class='pi'>・Omettre un です en fin de phrase retire également la politesse.</p>"+
+"<p class='pi'>・Le は dans la négation est en réalité la particule は on la pronnonce donc ' wa '.</p>"+
+"<p class='pi'>・Dans sa forme négative le じゃ est une contraction de では utilisé courrament à l'oral.</p>"+
 "<br><h3>exemple #1</h3>"+
 "<div class='ex'>今日は暑い<span class='b'>です</span>ね。</div>"+
 "<div class='hira'>きょうはあついですね。</div>"+
@@ -49,4 +51,14 @@ document.getElementById('content').innerHTML = ""+
 "<div class='ex'>子どものとき、甘いものが好き<span class='b'>でした</span>。</div>"+
 "<div class='hira'>こどものとき、あまいものがすきでした。</div>"+
 "<div class='trad'>Quand j'étais petits j'aimais les sucreries.</div>"+
+"<br><h3>exemple #8</h3>"+
+"<div class='ex'>私は日本人<span class='b'>じゃない</span>よ。</div>"+
+"<div class='hira'>わたしはにほんじんじゃないよ。</div>"+
+"<div class='trad'>Je ne suis pas japonais.</div>"+
+"<br><h3>exemple #9</h3>"+
+"<div class='ex'>そんなつもり<span class='b'>じゃなかった</span>。</div>"+
+"<div class='trad'>Je n'en avais pas l'intention.</div>"+
+"<br><h3>exemple #10</h3>"+
+"<div class='ex'>ごめんなさい！わざと<span class='b'>ではありませんでした</span>。</div>"+
+"<div class='trad'>Je suis désolé ! Je n'en avais pas l'intention.</div>"+
 "<br></div>";

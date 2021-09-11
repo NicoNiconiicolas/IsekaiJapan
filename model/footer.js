34ff6eecd5445061407ad82c25982a4e6c9cb88b
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-11 14:24:10 +0200

    Add files via upload

--- model/footer.js
+++ model/footer.js
@@ -1,10 +1,8 @@
 document.getElementsByTagName('body')[0].innerHTML += "<div id='footer'>"+
 "<table>"+
-"<tr><th>Jlpt</th><th>Pages</th><th>Contacts</th></tr>"+
-"<tr><td onclick='goto(\"n5\")'>N5</td><td onclick='goto(\"home\")'>Accueil</td><td>Mail</td></tr>"+
-"<tr><td onclick='goto(\"404\")'>N4</td><td onclick='goto(\"initiation\")'>Initiation</td></tr>"+
-"<tr><td onclick='goto(\"404\")'>N3</td><td onclick='goto(\"outils\")'>Outils</td></tr>"+
-"<tr><td onclick='goto(\"404\")'>N2</td></tr>"+
-"<tr><td onclick='goto(\"404\")'>N1</td></tr>"+
+"<tr><th colspan='2'>Jlpt</th><th>Pages</th><th>Contacts</th></tr>"+
+"<tr><td onclick='goto(\"n5\")'>N5</td><td onclick='goto(\"404\")'>N4</td><td onclick='goto(\"home\")'>Accueil</td><td>Mail</td></tr>"+
+"<tr><td onclick='goto(\"404\")'>N3</td><td onclick='goto(\"404\")'>N2</td><td onclick='goto(\"initiation\")'>Initiation</td></tr>"+
+"<tr><td onclick='goto(\"404\")'>N1</td><td></td><td onclick='goto(\"outils\")'>Outils</td></tr>"+
 "</table>"+
 "</div>";

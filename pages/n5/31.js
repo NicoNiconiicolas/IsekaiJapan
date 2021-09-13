3d3dda8ae1937daeb1dc7a9db5b300886448b78a
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-13 20:20:23 +0200

    Add files via upload

--- pages/n5/31.js
+++ pages/n5/31.js
@@ -1,5 +1,42 @@
 document.getElementById('content').innerHTML = ""+
-"<div id='main'>"+
-"<h1>文法<br>grammaire</h1>"+
-"<h2>Prochainement...</h2>"+
-"</div>";
+"<div id='main'><div id='num'></div>"+
+"<h1>けれでども<br>keredomo</h1>"+
+"<br><p><span class='gbox'>Sens</span></p><br>"+
+"<p>mais, cependant, même si</p>"+
+"<br><p><span class='gbox'>Utilisation</span></p><br>"+
+"<p>à utiliser entre 2 idées contradictoires</p>"+
+"<br><p class='pi'>・Version plus formelle de <a class='inlink' href='?p=n5/30'>けど</a></p>"+
+"<p class='pi'>・du moins formel au plus formel: けど、けれど、けれども</p>"+
+"<br><h3>exemple #1</h3>"+
+"<div class='ex'>このカメラは高かった<span class='b'>けれど</span>、すぐ壊れてしまいました。</div>"+
+"<div class='hira'>このカメラはたかかったけれど、すぐこわれてしまいました。</div>"+
+"<div class='trad'>Cette caméra était chère, mais je l'ai tout de suite cassée. (par accident)</div>"+
+"<br><h3>exemple #2</h3>"+
+"<div class='ex'>外はさむい<span class='b'>けれど</span>、家の中は暖かいです。</div>"+
+"<div class='hira'>そとはさむいけれど、いえのなかはあたたかいです。</div>"+
+"<div class='trad'>Il fait froid dehors, mais il fait chaud à l'interieur de la maison.</div>"+
+"<br><h3>exemple #3</h3>"+
+"<div class='ex'>眠い<span class='b'>けれど</span>、まだ宿題があるから寝られません。</div>"+
+"<div class='hira'>ねむいけれど、まだしゅくだいがあるからねられません。</div>"+
+"<div class='trad'>J'ai sommeil, mais j'ai encore des devoirs donc je ne ne peux pas aller me coucher.</div>"+
+"<br><h3>exemple #4</h3>"+
+"<div class='ex'>雪が降っている<span class='b'>けれども</span>、スーパーに行きます。</div>"+
+"<div class='hira'>ゆきがふっているけれども、スーパーにいきます。</div>"+
+"<div class='trad'>Même s'il neige, je vais au supermarché.</div>"+
+"<br><h3>exemple #5</h3>"+
+"<div class='ex'>お金はあまり持っていません。<span class='b'>けれども</span>、毎日幸せです。</div>"+
+"<div class='hira'>おかねはあまりもっていません。けれども、まいにちしあわせです。</div>"+
+"<div class='trad'>Je n'ai pas vraiment d'argent, mais je suis heureux tous les jours.</div>"+
+"<br><h3>exemple #6</h3>"+
+"<div class='ex'>先生はクラスでは厳しい<span class='b'>けれど</span>、クラスの外ではやさしいです。</div>"+
+"<div class='hira'>せんせいはクラスではきみしいけれど、くらすのそとではやさしいです。</div>"+
+"<div class='trad'>Mon professeur est strict en classe, mais gentil à en dehors des cours.</div>"+
+"<br><h3>exemple #7</h3>"+
+"<div class='ex'>彼は若い<span class='b'>けれども</span>経験がある。</div>"+
+"<div class='hira'>かれはわかいけれどもけいけんがある。</div>"+
+"<div class='trad'>Il est jeune, mais il a de l'expérience.</div>"+
+"<br><h3>exemple #8</h3>"+
+"<div class='ex'>列車が遅れた<span class='b'>けれども</span>、なんとか間に合った。</div>"+
+"<div class='hira'>れっしゃがおくれたけれども、なんとかまにあった。</div>"+
+"<div class='trad'>Bien que le train était en retard, d'une façon ou d'une autre il est arrivé à l'heure.</div>"+
+"<br></div>";

4a49117c3384b37a19f3b4b18e7f799794a7118f
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-13 15:54:49 +0200

    Add files via upload

--- pages/outils.js
+++ pages/outils.js
@@ -3,6 +3,6 @@ document.getElementById('content').innerHTML = ""+
 	"<h1>"+
 		"Outils"+
 	"</h1>"+
-	"<a href='https://niconiconiicolas.github.io/Reibun/' target='_blank' class='black'>Reibun | recherche d'exemples</a>"+
-	"<a href='https://niconiconiicolas.github.io/Anko/' target='_blank'class='brown'>Anko | importer ses decks anki sur bot kotoba (discord)</a>"+
+	"<a href='https://niconiconiicolas.github.io/reibun/' class='black'>Reibun | recherche d'exemples</a>"+
+	"<a href='https://niconiconiicolas.github.io/Anko/'class='brown'>Anko | importer ses decks anki sur bot kotoba (discord)</a>"+
 "</div>";

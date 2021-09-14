d41426988050f80dd539e89974088a0d15f9d0b9
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-15 00:23:08 +0200

    Add files via upload

--- pages/n5/32.js
+++ pages/n5/32.js
@@ -1,5 +1,49 @@
 document.getElementById('content').innerHTML = ""+
-"<div id='main'>"+
-"<h1>文法<br>grammaire</h1>"+
-"<h2>Prochainement...</h2>"+
-"</div>";
+"<div id='main'><div id='num'></div>"+
+"<h1>まだ<br>mada</h1>"+
+"<br><p><span class='gbox'></span>Sens</p><br>"+
+"<p>encore (en train de), toujours, pas encore, toujours pas</p>"+
+"<br><p><span class='gbox'>Utilisation</span></p><br>"+
+"<p>まだ + verbe (présent ou passé continu)</p>"+
+"<p>まだ + nom commun</p>"+
+"<p class='pi'>・<span class='b'>まだ</span> exprime une continuité</p>"+
+"<p class='pi'>・Souvent confondu avec <a class='inlink' href='?p=404'>また</a>(répétition, occurence)</p>"+
+"<p class='pi'>・existe aussi sous la forme <span class='b'>まだまだ</span> pour donner plus d'emphase</p>"+
+"<br><h3>exemple #1</h3>"+
+"<div class='ex'><span class='b'>まだ</span>寝ているの？</div>"+
+"<div class='hira'>まだねているの？</div>"+
+"<div class='trad'>Tu es encore en train de dormir ?</div>"+
+"<br><h3>exemple #2</h3>"+
+"<div class='ex'><span class='b'>まだ</span>高校生です。</div>"+
+"<div class='hira'>まだこうこうせいです。</div>"+
+"<div class='trad'>Je suis encore lycéen.</div>"+
+"<br><h3>exemple #3</h3>"+
+"<div class='ex'><span class='b'>まだ</span>ですか？</div>"+
+"<div class='trad'>C'est toujours pas prêt ?</div>"+
+"<div class='trad'>On est toujours par arrivé ?</div>"+
+"<div class='trad'>Toujours pas...?</div>"+
+"<br><h3>exemple #4</h3>"+
+"<div class='ex'><span class='b'>まだまだ</span>暑いだ。</div>"+
+"<div class='hira'>まだまだあついだ。</div>"+
+"<div class='trad'>Il fait toujours chaud.</div>"+
+"<br><h3>exemple #5</h3>"+
+"<div class='ex'>先生は<span class='b'>まだ</span>来ていない。</div>"+
+"<div class='hira'>せんせいはまだきていない。</div>"+
+"<div class='trad'>Le professeur n'est toujours pas arrivé.</div>"+
+"<br><h3>exemple #6</h3>"+
+"<div class='ex'>もう10時ですが弟が<span class='b'>まだ</span>寝ている。</div>"+
+"<div class='hira'>もうじゅうじですがおとうとがまだねている。</div>"+
+"<div class='trad'>Il est déjà 10h mais mon petit frère est encore en train de dormir.</div>"+
+"<br><h3>exemple #7</h3>"+
+"<div class='ex'><span class='b'>まだ</span>1時間もあるよ。</div>"+
+"<div class='hira'>まだいちじかんもあるよ。</div>"+
+"<div class='trad'>Il reste encore 1 heure.</div>"+
+"<br><h3>exemple #8</h3>"+
+"<div class='ex'>あきらめるのは<span class='b'>まだ</span>早い！</div>"+
+"<div class='hira'>あきらめるのはまだはやい！</div>"+
+"<div class='trad'>Il est encore trop tôt pour abandonner !</div>"+
+"<br><h3>exemple #9</h3>"+
+"<div class='ex'>私は宿題が<span class='b'>まだ</span>終わっていません。</div>"+
+"<div class='hira'>わたしはしゅくだいがまだおわっていません。</div>"+
+"<div class='trad'>Je n'ai toujours pas fini mes devoirs.</div>"+
+"<br></div>";

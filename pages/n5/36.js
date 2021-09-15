f1331ecb04410c5bf58ab0a00de9bc8cd130127c
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-15 15:52:13 +0200

    Add files via upload

---
+++ pages/n5/36.js
@@ -0,0 +1,46 @@
+document.getElementById('content').innerHTML = ""+
+"<div id='main'><div id='num'></div>"+
+"<h1>ませんか<br>masen ka</h1>"+
+"<br><p><span class='gbox'>Sens</span></p><br>"+
+"<p>voudrais-tu, aimerais-tu, pourrais-tu, devrions-nous</p>"+
+"<br><p><span class='gbox'>Utilisation</span></p><br>"+
+"<p>verbe (forme ません) + か</p>"+
+"<p class='conj'>・Ce point de grammaire est souvent utilisée lorsque l'on invite ou fait une proposition à quelqu'un. Une question trop directe sera souvent perçue comme malpolie, veillez alors à utiliser une tournure de phrase en ませんか</p>"+
+"<br><h3>exemple #1</h3>"+
+"<div class='ex'>行き<span class='b'>ませんか</span>。</div>"+
+"<div class='hira'>いきませんか。</div>"+
+"<div class='trad'>Devrions-nous y aller ?</div>"+
+"<div class='trad'>Voudrais-tu y aller ?</div>"+
+"<br><h3>exemple #2</h3>"+
+"<div class='ex'>今日一緒に食べ<span class='b'>ません</span>か？</div>"+
+"<div class='hira'>きょういっしょにたべませんか？</div>"+
+"<div class='trad'>Aimerais-tu que nous mangions ensemble aujourd'hui ?</div>"+
+"<br><h3>exemple #3</h3>"+
+"<div class='ex'>うちで映画を見<span class='b'>ませんか</span>。</div>"+
+"<div class='hira'>うちでえいがをみませんか。</div>"+
+"<div class='trad'>Aimerais-tu regarder un film chez moi ?</div>"+
+"<br><h3>exemple #4</h3>"+
+"<div class='ex'>お茶にし<span class='b'>ませんか</span>？</div>"+
+"<div class='hira'>おちゃにしませんか？</div>"+
+"<div class='trad'>Voudrais-tu boire du thé ?</div>"+
+"<br><h3>exemple #5</h3>"+
+"<div class='ex'>日曜日にテニスをし<span class='b'>ませんか</span>。</div>"+
+"<div class='hira'>にちようびにテニスをしませんか。</div>"+
+"<div class='trad'>Est-ce que ça te plairais de jouer au tenis ce dimanche ?</div>"+
+"<br><h3>exemple #6</h3>"+
+"<div class='ex'>明日自転車で学校へ行き<span class='b'>ませんか</span>？</div>"+
+"<div class='hira'>あしたじてんしゃでがっこうへいきませんか？</div>"+
+"<div class='trad'>Voudrais-tu aller à l'école en vélo demain ?</div>"+
+"<br><h3>exemple #7</h3>"+
+"<div class='ex'>もう暗くなりました。家へ帰り<span class='b'>ませんか</span>。</div>"+
+"<div class='hira'>もうくらくなりました。いえへかえりませんか。</div>"+
+"<div class='trad'>Il commence déjà à faire noir, ne rentrerions nous pas à la maison ?</div>"+
+"<br><h3>exemple #8</h3>"+
+"<div class='ex'>写真をとってくれ<span class='b'>ませんか</span>？</div>"+
+"<div class='hira'>しゃしんをとってくれませんか？</div>"+
+"<div class='trad'>Pourrais-tu prendre une photo ?</div>"+
+"<br><h3>exemple #9</h3>"+
+"<div class='ex'>私の仕事を手伝ってくれ<span class='b'>ませんか</span>。</div>"+
+"<div class='hira'>わたしのしごとをてつだってくれませんか。</div>"+
+"<div class='trad'>Pourrais-tu m'aider dans mon travail ?</div>"+
+"<br></div>";
---
+++ pages/n5/37.js
@@ -0,0 +1,50 @@
+document.getElementById('content').innerHTML = ""+
+"<div id='main'><div id='num'></div>"+
+"<h1>ましょう<br>mashou</h1>"+
+"<br><p><span class='gbox'>Sens</span></p><br>"+
+"<p>Faisons, devrions-nous </p>"+
+"<br><p><span class='gbox'>Utilisation</span></p><br>"+
+"<p>racine du verbe (verbe sans ます) + ましょう</p>"+
+"<p class='pi'>・Utilisé pour faire des suggestions</p>"+
+"<p class='pi'>・En ajoutant か cela deviendra une question suggestive, voir <a href='?p=404' class='inlink'>ませんか</a></p>"+
+"<br><h3>exemple #1</h3>"+
+"<div class='ex'>行き<span class='b'>ましょう</span>！</div>"+
+"<div class='hira'>いきましょう！</div>"+
+"<div class='trad'>Allons-y !</div>"+
+"<br><h3>exemple #2</h3>"+
+"<div class='ex'>駅で会い<span class='b'>ましょう</span>！</div>"+
+"<div class='hira'>えきであいましょう！</div>"+
+"<div class='trad'>Rencontrons nous à la gare !</div>"+
+"<br><h3>exemple #3</h3>"+
+"<div class='ex'>もうご飯の時間だよ、早く食べ<span class='b'>ましょう</span>！</div>"+
+"<div class='hira'>もうごはんのじかんだよ、はやくたべましょう！</div>"+
+"<div class='trad'>C'est déjà l'heure du repas, dépêchons nous de manger !</div>"+
+"<br><h3>exemple #4</h3>"+
+"<div class='ex'>帰る前に教室をきれいにし<span class='b'>ましょう</span>。</div>"+
+"<div class='hira'>かえるまえにきょうしつをきれいにしましょう。</div>"+
+"<div class='trad'>Nettoyons la salle de classe avant de rentrer chez nous.</div>"+
+"<br><h3>exemple #5</h3>"+
+"<div class='ex'>一緒に日本語を勉強し<span class='b'>ましょう</span>！</div>"+
+"<div class='hira'>いっしょににほんごをべんきょうしよう！</div>"+
+"<div class='trad'>Étudions le japonais ensemble !</div>"+
+"<br><h3>exemple #6</h3>"+
+"<div class='ex'>もう暗くなったから、帰り<span class='b'>ましょう</span>。</div>"+
+"<div class='hira'>もうくらくなったから、かえりましょう。</div>"+
+"<div class='trad'>Il commence déjà à faire noir, rentrons à la maison.</div>"+
+"<br><h3>exemple #7</h3>"+
+"<div class='ex'>来週、すしを食べに行き<span class='b'>ましょう</span>よ！</div>"+
+"<div class='hira'>らいしゅう、すしをたべにいきましょうよ。</div>"+
+"<div class='trad'>La semaine prochaine, allons manger des sushis !</div>"+
+"<br><h3>exemple #8</h3>"+
+"<div class='ex'>10時になったら、出かけ<span class='b'>ましょう</span>。</div>"+
+"<div class='hira'>10じになったら、でかけましょう。</div>"+
+"<div class='trad'>Sortons quand il sera 10h.</div>"+
+"<br><h3>exemple #9</h3>"+
+"<div class='ex'>電気をつけて、部屋を明るくし<span class='b'>ましょう</span>。</div>"+
+"<div class='hira'>でんきをつけて、へやをあかるくしましょう。</div>"+
+"<div class='trad'>Allumons la lumière pour éclairer la pièce.</div>"+
+"<br><h3>exemple #10</h3>"+
+"<div class='ex'>決める前にもっとデータを収集し<span class='b'>ましょう</span>。</div>"+
+"<div class='hira'>きめるまえにもっとデータをしゅうしゅうしましょう。</div>"+
+"<div class='trad'>Récoltons plus de données avant de nous décider.</div>"+
+"<br></div>";

4f83f947c171ab13679b18151fa5e1514734ec28
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-09-15 12:27:17 +0200

    Add files via upload

---
+++ pages/n5/34.js
@@ -0,0 +1,62 @@
+document.getElementById('content').innerHTML = ""+
+"<div id='main'><div id='num'></div>"+
+"<h1>もう<br>mou</h1>"+
+"<br><p><span class='gbox'>Sens</span></p><br>"+
+"<p>1) déjà</p>"+
+"<div class='ex'><span class='b'>もう</span>9時だ。</div>"+
+"<div class='hira'>もうくじだ。</div>"+
+"<div class='trad'>il est déjà 9h.</div>"+
+"<p>2) plus</p>"+
+"<div class='ex'><span class='b'>もう</span>行かない。</div>"+
+"<div class='hira'>もういかない</div>"+
+"<div class='trad'>Je n'y vais plus.</div>"+
+"<p>3) encore, de nouveau, une autre fois</p>"+
+"<div class='ex'><span class='b'>もう</span>二度と行かない。</div>"+
+"<div class='hira'>もうにどとのいかない。</div>"+
+"<div class='trad'>Je n'irai pas un seconde fois.</div>"+
+"<p>4) exprime l'irritation</p>"+
+"<div class='ex'><span class='b'>もう</span>たくさんだ。</div>"+
+"<div class='trad'>J'en ai plus qu'assez.</div>"+
+"<br><p><span class='gbox'>Utilisation</span></p><br>"+
+"<p>voir exemples ci-dessus</p>"+
+"<br><h3>exemple #1</h3>"+
+"<div class='ex'><span class='b'>もう</span>昼ご飯の時間だ。早く食べましょう！</div>"+
+"<div class='hira'>もうひるごはんのじかんだ。はやくたべましょう！</div>"+
+"<div class='trad'>C'est déjà l'heure du déjeuner. Dépêchons nous de manger !</div>"+
+"<br><h3>exemple #2</h3>"+
+"<div class='ex'>宿題は<span class='b'>もう</span>終わった？</div>"+
+"<div class='hira'>しゅくだいはもうおわった？</div>"+
+"<div class='trad'>Tu as déjà terminé tes devoirs ?</div>"+
+"<br><h3>exemple #3</h3>"+
+"<div class='ex'>ビール<span class='b'>もう</span>一杯ください！</div>"+
+"<div class='hira'>ビールもういっぱいください！</div>"+
+"<div class='trad'>Encore une bière s'il vous plaît !</div>"+
+"<br><h3>exemple #4</h3>"+
+"<div class='ex'>飲み物は全部飲みました。<span class='b'>もう</span>ありません。</div>"+
+"<div class='hira'>のみものはぜんぶのみました。もうありません。</div>"+
+"<div class='trad'>Nous avons bu toutes les boissons. Il n'y en a plus.</div>"+
+"<br><h3>exemple #5</h3>"+
+"<div class='ex'><span class='b'>もう</span>少し待ってください。</div>"+
+"<div class='hira'>もうすこしまってください。</div>"+
+"<div class='trad'>Veuillez attendre un peu plus longtemps.</div>"+
+"<br><h3>exemple #6</h3>"+
+"<div class='ex'>コーヒーは<span class='b'>もう</span>飲まないことにしました。</div>"+
+"<div class='hira'>コーヒーはもうのまないことにしました。</div>"+
+"<div class='trad'>J'ai décidé de ne plus boire de café.</div>"+
+"<br><h3>exemple #7</h3>"+
+"<div class='ex'><span class='b'>もう</span>この暑さには、耐えられない。</div>"+
+"<div class='hira'>もうこのあつかには、たえられない。</div>"+
+"<div class='trad'>Je ne peux plus supporter cette chaleur.</div>"+
+"<br><h3>exemple #8</h3>"+
+"<div class='ex'><span class='b'>もう</span>我慢できない。</div>"+
+"<div class='hira'>もうがまんできない。</div>"+
+"<div class='trad'>Je n'arrive plus à me retenir.</div>"+
+"<br><h3>exemple #9</h3>"+
+"<div class='ex'><span class='b'>もう</span>10時ですが、彼はまだ寝ています。</div>"+
+"<div class='hira'>もう10じですが、かれはまだねています。</div>"+
+"<div class='trad'>Il est déjà 10h, mais il est encore occupé à dormir.</div>"+
+"<br><h3>exemple #10</h3>"+
+"<div class='ex'>彼女と結婚してから、<span class='b'>もう</span>５年になります。</div>"+
+"<div class='hira'>かのじょとけっこんしてから、もう５ねんになります。</div>"+
+"<div class='trad'>Ça fait déjà 5 ans qu'elle s'est mariée.</div>"+
+"<br></div>";

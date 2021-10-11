cba2dc03ede2c4b397f864a89c46f9172de1f084
Xuejiu <35692364+NicoNiconiicolas@users.noreply.github.com>
2021-10-11 11:26:26 +0200

    Add files via upload

---
+++ pages/n4/1.js
@@ -0,0 +1,5 @@
+document.getElementById('content').innerHTML = ""+
+"<div id='main'>"+
+"<h1>文法<br>grammaire</h1>"+
+"<p class='conj'>Prochainement...</p>"+
+"</div>";
---
+++ pages/n4/goi1.js
@@ -0,0 +1,5 @@
+document.getElementById('content').innerHTML = ""+
+"<div id='main'>"+
+"<h1>語彙<br>vocabulaire</h1>"+
+"<p class='conj'>Prochainement...</p>"+
+"</div>";

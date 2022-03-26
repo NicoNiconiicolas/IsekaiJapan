98d1556c3d4c00d3030de53af434c6dc8bede9a6
Kyu <35692364+budouen@users.noreply.github.com>
2022-03-26 18:10:57 +0100

    Add files via upload

--- model/footer.js
+++ model/footer.js
@@ -2,7 +2,7 @@ document.getElementsByTagName('body')[0].innerHTML += "<div id='footer'>"+
 "<table>"+
 "<tr><th colspan='2'>Jlpt</th><th>Pages</th><th>Contacts</th></tr>"+
 "<tr><td onclick='goto(\"n5\")'>N5</td><td onclick='goto(\"404\")'>N4</td><td onclick='goto(\"home\")'>Accueil</td><td>Mail</td></tr>"+
-"<tr><td onclick='goto(\"404\")'>N3</td><td onclick='goto(\"404\")'>N2</td><td onclick='goto(\"initiation\")'>Initiation</td></tr>"+
-"<tr><td onclick='goto(\"404\")'>N1</td><td></td><td onclick='goto(\"outils\")'>Outils</td></tr>"+
+"<tr><td onclick='goto(\"404\")'>N3</td><td onclick='goto(\"404\")'>N2</td><td onclick='goto(\"initiation\")'>Introduction</td></tr>"+
+"<tr><td onclick='goto(\"404\")'>N1</td><td style='background:#151515!important'></td><td onclick='goto(\"outils\")'>Outils</td></tr>"+
 "</table>"+
 "</div>";
